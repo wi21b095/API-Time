86efb78f1600a6f44a2dd0fbbfab327ed946a0f8
wi21b095 <wi21b095@technikum-wien.at>
2022-06-22 18:47:53 +0200

    myTime hinzugefügt

--- src/main/java/com/example/apitime/API_Time_Controller.java
+++ src/main/java/com/example/apitime/API_Time_Controller.java
@@ -4,16 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 
 @RestController
 
 public class API_Time_Controller {
-    @GetMapping("/sum")
+    @GetMapping("/time")
 
-    public int sum(@RequestParam int a, @RequestParam int b) {
-        return a + b;
+    public LocalTime time () {
+        LocalTime myTime = LocalTime.now();
+        return myTime;
+
     }
 
     /*
