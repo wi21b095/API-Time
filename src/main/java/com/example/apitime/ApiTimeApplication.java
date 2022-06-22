012fb9b38fbf96f77351a65c4d683e34de2927dd
wi21b095 <wi21b095@technikum-wien.at>
2022-06-22 18:07:46 +0200

    Initial commit

---
+++ src/main/java/com/example/apitime/ApiTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.apitime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apitime/ApiTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apitime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
